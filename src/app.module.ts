4871ee5f7ac8b3356bbd09dbaac7b170e71be673
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-08-22 03:25:36 +0900

    처음부터 다시 작성해보기, 시작

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // decorator - 클래스에 함수 기능을 추가
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
