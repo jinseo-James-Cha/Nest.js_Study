d9773f355183212959cc6ec06bc692ec607538b1
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-08-22 17:23:12 +0900

    2021-08-22 service layer added, and Read done

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+
+    private movies: Movie[] = []; // 변수선언 순서
+                                  // 접근제한자 변수명 : 변수 타입 = 값초기화;
+
+    getAll(): Movie[]{
+        return this.movies;
+    }
+
+    getOne(id:string): Movie{
+        return this.movies.find(movie => movie.id == parseInt(id)); // string id의 int형변환 1
+        // this.movies.find(movie => movie.id == +id); // string id의 int형변환 2
+    }
+}
