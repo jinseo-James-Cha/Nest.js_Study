aee2d4959ab61ad6f4426c8985373eb4a8923b3b
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-08-22 16:25:35 +0900

    all settings done in Mac

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 // decorator - 클래스에 함수 기능을 추가
 @Module({
   imports: [],
   controllers: [MoviesController],
-  providers: [],
+  providers: [MoviesService],
 })
 
 export class AppModule {}
