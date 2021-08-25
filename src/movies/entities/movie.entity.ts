06cf77a51abc3541ffe2f152c2fb189ca3f2a90e
jinseo-James-Cha <jinseo_cha@hotmail.com>
2021-08-25 23:40:19 +0900

    2021-08-25 nest study started

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,6 +1,6 @@
-export class Movie{
-    id: number;
-    title: string;
-    year: number;
-    genres: string[];
+export class Movie {
+  id: number;
+  title: string;
+  year: number;
+  genres: string[];
 }
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,55 +1,62 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { NotFoundException } from '@nestjs/common/exceptions/not-found.exception';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies') // entry point
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
 
-    constructor(private readonly moviesService: MoviesService){} 
+  @Get()
+  getAll(): Movie[] {
+    // return 'this will return all movies';
+    return this.moviesService.getAll();
+  }
 
-    @Get()
-    getAll(): Movie[]{
-        // return 'this will return all movies';
-        return this.moviesService.getAll();
-    }
+  @Get('search') // http://localhost:3000/movies/search?year=2003
+  search(@Query('year') searchingYear: string) {
+    // query string의 name값 -> @Query('name') 사용해야한다
+    // return `We are search for a movie made after : ${searchingYear}`;
+  }
 
-    @Get('search') // http://localhost:3000/movies/search?year=2003
-    search(@Query('year') searchingYear: string){ // query string의 name값 -> @Query('name') 사용해야한다
-        // return `We are search for a movie made after : ${searchingYear}`;
+  @Get(':id') // :id -> Spring의 PathVariable과 같다. http://localhost:3000/movies/1
+  getOne(@Param('id') movieId: string): Movie {
+    // url로 부터 들어온 parameter를 읽어오는 decorator : @Param('/:parametername')
+    // return `this will return one movie with the id  : ${movieId}`; // ''이나 ""가 아닌 `${movieId}`가 사용되였다.
+    return this.moviesService.getOne(movieId);
+  }
 
-    }
+  @Post()
+  create(@Body() movieData: string) {
+    console.log(movieData);
+    // return 'This will create a movie';
+    return this.moviesService.create(movieData);
+  }
 
-    @Get(':id') // :id -> Spring의 PathVariable과 같다. http://localhost:3000/movies/1
-    getOne(@Param('id') movieId: string): Movie{  // url로 부터 들어온 parameter를 읽어오는 decorator : @Param('/:parametername')
-        // return `this will return one movie with the id  : ${movieId}`; // ''이나 ""가 아닌 `${movieId}`가 사용되였다. 
-        return this.moviesService.getOne(movieId);
-    }
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    // return `This will delete a movie with the id :  ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
+  }
 
-    @Post()
-    create(@Body() movieData: string){
-        console.log(movieData);
-        // return 'This will create a movie';
-        return this.moviesService.create(movieData);
-    }
+  @Patch('/:id')
+  ptch(@Param('id') movieId: string, @Body() updateData) {
+    // body로 부터 들어온 json을 읽어오는 decorator : @Body
+    // return { // return문을 JSON으로 만들어서 응답한다.
+    //     updatedMovie : movieId,
+    //     ...updateData, // ...의 의미는 이미updateDate는 JSON포멧이라서?
+    // };
 
-    @Delete("/:id")
-    remove(@Param('id') movieId: string){
-        // return `This will delete a movie with the id :  ${movieId}`;
-        return this.moviesService.deleteOne(movieId);
-    }
-
-    @Patch('/:id')
-    ptch(@Param('id') movieId: string, @Body() updateData){ // body로 부터 들어온 json을 읽어오는 decorator : @Body
-        // return { // return문을 JSON으로 만들어서 응답한다.
-        //     updatedMovie : movieId,
-        //     ...updateData, // ...의 의미는 이미updateDate는 JSON포멧이라서?
-        // };
-
-        return this.moviesService.update(movieId, updateData);
-    }
-
-    
-
-
+    return this.moviesService.update(movieId, updateData);
+  }
 }
